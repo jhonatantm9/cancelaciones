8e3c089f0679ac04bcf87950ccabefc92c036ac4
Jhonatan <jhonatan.tamayom@udea.edu.co>
2023-04-25 21:40:44 -0500

    primer commit

---
+++ src/main/java/com/udea/cancelaciones/CancelacionesApplication.java
@@ -0,0 +1,13 @@
+package com.udea.cancelaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CancelacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CancelacionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udea/cancelaciones/CancelacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.cancelaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CancelacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
