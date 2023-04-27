445cc1a24988aeafc671a5bc88660267f64c8787
Jhonatan <jhonatan.tamayom@udea.edu.co>
2023-04-27 13:48:45 -0500

    prueba para tests

---
+++ src/main/java/com/udea/cancelaciones/HolaMundo.java
@@ -0,0 +1,15 @@
+package com.udea.cancelaciones;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class HolaMundo {
+    @GetMapping("/")
+    public String saludar(){
+        return("Hola mundo");
+    }
+    
+}
---
+++ src/test/java/com/udea/cancelaciones/models/EstudianteModelTest.java
@@ -0,0 +1,49 @@
+package com.udea.cancelaciones.models;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class EstudianteModelTest {
+    @Test
+    void testGetNombre() {
+        String nombre = "David";
+        EstudianteModel estudiante = new EstudianteModel("CC", 123, nombre);
+        assertEquals(nombre, estudiante.getNombre());
+    }
+
+    @Test
+    void testGetNumeroDocumento() {
+        int documento = 9876;
+        EstudianteModel estudiante = new EstudianteModel("CC", documento, "David");
+        assertEquals(documento, estudiante.getNumeroDocumento());
+    }
+
+    @Test
+    void testGetTipoDocumento() {
+        String tipoDocumento = "CC";
+        EstudianteModel estudiante = new EstudianteModel(tipoDocumento, 123, "David");
+        assertEquals(tipoDocumento, estudiante.getTipoDocumento());
+    }
+
+    @Test
+    void testSetNombre() {
+        EstudianteModel estudiante = new EstudianteModel("CC", 123, "David");
+        estudiante.setNombre("Roberto");
+        assertEquals("Roberto", estudiante.getNombre());
+    }
+
+    @Test
+    void testSetNumeroDocumento() {
+        EstudianteModel estudiante = new EstudianteModel("CC", 123, "David");
+        estudiante.setNumeroDocumento(789);
+        assertEquals(789, estudiante.getNumeroDocumento());
+    }
+
+    @Test
+    void testSetTipoDocumento() {
+        EstudianteModel estudiante = new EstudianteModel("CC", 123, "David");
+        estudiante.setTipoDocumento("TI");
+        assertEquals("TI", estudiante.getTipoDocumento());
+    }
+}
