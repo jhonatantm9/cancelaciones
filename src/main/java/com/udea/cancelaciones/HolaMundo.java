090d6f0b6b612bc113c6fbfffefc07cc1020d0de
Jhonatan <jhonatan.tamayom@udea.edu.co>
2023-04-28 14:35:11 -0500

    "Prueba tests"

--- src/main/java/com/udea/cancelaciones/HolaMundo.java
+++ src/main/java/com/udea/cancelaciones/HolaMundo.java
@@ -1,7 +1,6 @@
 package com.udea.cancelaciones;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 //Clase prueba
