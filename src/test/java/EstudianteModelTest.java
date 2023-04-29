422771ff4476eb6db3f65eaba666de895e3cec7e
Jhonatan <jhonatan.tamayom@udea.edu.co>
2023-04-28 22:25:05 -0500

    probando quality gate

--- src/test/java/EstudianteModelTest.java
+++ src/test/java/EstudianteModelTest.java
@@ -14,38 +14,38 @@ public class EstudianteModelTest {
         assertEquals(nombre, estudiante.getNombre());
     }
 
-    @Test
-    void testGetNumeroDocumento() {
-        int documento = 9876;
-        EstudianteModel estudiante = new EstudianteModel("CC", documento, "David");
-        assertEquals(documento, estudiante.getNumeroDocumento());
-    }
+    // @Test
+    // void testGetNumeroDocumento() {
+    //     int documento = 9876;
+    //     EstudianteModel estudiante = new EstudianteModel("CC", documento, "David");
+    //     assertEquals(documento, estudiante.getNumeroDocumento());
+    // }
 
-    @Test
-    void testGetTipoDocumento() {
-        String tipoDocumento = "CC";
-        EstudianteModel estudiante = new EstudianteModel(tipoDocumento, 123, "David");
-        assertEquals(tipoDocumento, estudiante.getTipoDocumento());
-    }
+    // @Test
+    // void testGetTipoDocumento() {
+    //     String tipoDocumento = "CC";
+    //     EstudianteModel estudiante = new EstudianteModel(tipoDocumento, 123, "David");
+    //     assertEquals(tipoDocumento, estudiante.getTipoDocumento());
+    // }
 
-    @Test
-    void testSetNombre() {
-        EstudianteModel estudiante = new EstudianteModel("CC", 123, "David");
-        estudiante.setNombre("Roberto");
-        assertEquals("Roberto", estudiante.getNombre());
-    }
+    // @Test
+    // void testSetNombre() {
+    //     EstudianteModel estudiante = new EstudianteModel("CC", 123, "David");
+    //     estudiante.setNombre("Roberto");
+    //     assertEquals("Roberto", estudiante.getNombre());
+    // }
 
-    @Test
-    void testSetNumeroDocumento() {
-        EstudianteModel estudiante = new EstudianteModel("CC", 123, "David");
-        estudiante.setNumeroDocumento(789);
-        assertEquals(789, estudiante.getNumeroDocumento());
-    }
+    // @Test
+    // void testSetNumeroDocumento() {
+    //     EstudianteModel estudiante = new EstudianteModel("CC", 123, "David");
+    //     estudiante.setNumeroDocumento(789);
+    //     assertEquals(789, estudiante.getNumeroDocumento());
+    // }
 
-    @Test
-    void testSetTipoDocumento() {
-        EstudianteModel estudiante = new EstudianteModel("CC", 123, "David");
-        estudiante.setTipoDocumento("TI");
-        assertEquals("TI", estudiante.getTipoDocumento());
-    }
+    // @Test
+    // void testSetTipoDocumento() {
+    //     EstudianteModel estudiante = new EstudianteModel("CC", 123, "David");
+    //     estudiante.setTipoDocumento("TI");
+    //     assertEquals("TI", estudiante.getTipoDocumento());
+    // }
 }
